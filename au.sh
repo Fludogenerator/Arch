ef8e9b7a184f4744b516e4459cd000c90bbc99d5
Fludogenerator <morevua89@mail.ru>
2024-06-21 20:06:22 +0700

    Update au.sh

--- au.sh
+++ au.sh
@@ -2,3 +2,7 @@
 
 # Определить букву диска
 disk=$(lsblk -po "model,name" | grep -e HARD | cut -d'/' -f3)
+
+# Обновление зеркал
+reflector --country ru --age 24 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+pacman -Sy
