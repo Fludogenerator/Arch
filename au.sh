5545feb16724fb0b0570fa077658197a82efc30b
Fludogenerator <morevua89@mail.ru>
2024-06-21 20:51:06 +0700

    Update au.sh

--- au.sh
+++ au.sh
@@ -3,6 +3,22 @@
 # Определить букву диска
 disk=$(lsblk -po "model,name" | grep -e HARD | cut -d'/' -f3)
 
+# Разметка диска (GPT/MBR)
+echo -ne 'label:gpt\nsize=1024M,type=U\nsize=23552M,type=L\n' | sfdisk /dev/$disk
+echo -ne 'label:mbr\nsize=1024M,type=L,bootable\nsize=23552M,type=L\n' | sfdisk /dev/$disk
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Обновление зеркал
 reflector --country ru --age 24 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 pacman -Sy
